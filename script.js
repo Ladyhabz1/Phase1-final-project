895a97227e74cb8dc4c580b14f36ba54baa23359
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2024-12-19 13:13:32 +0300

    Bug fixes

--- script.js
+++ script.js
@@ -78,21 +78,20 @@ function handleSearch(product){
                 document.querySelector(".container").innerHTML = "";
 
                 if(filterProduct.length === 0){
-                    document.querySelector(".container").innerHTML = "";
-                    <p>No product matches found for ${querry}</p>
+                    document.querySelector(".container").innerHTML = `
+                    <p>No product matches found for ${querry}</p>`
                 } else {
-                    filterProduct.forEach((product) =>  renderProduct());
+                    filterProduct.forEach((product) =>  renderProduct(product));
                 }
             })
             .catch(Error => console.error("Error fetching the search", Error));
             
-    })
-
-
+    });
 }
 
 //Inititalize the application
 document.addEventListener("DOMContentLoaded", () => {
+    //// Ensure the ul#products element exists
     if(!document.querySelector("#products")){
         const productList = document.createElement("ul");    
         productList.id = "products"   
@@ -129,17 +128,22 @@ document.addEventListener("DOMContentLoaded", () => {
     document.head.appendChild(style);
 
     //check if the required DOM exists if not create them
-    if (!document.querySelector(".container")) {
-        const container = document.createElement("div");
-        container.className = "container";
-        document.body.appendChild(container);
+
+    if (!document.querySelector(".search-bar")) {
+        const searchBar = document.createElement("input");
+        searchBar.className = "search-bar";
+        searchBar.type = "text";
+        searchBar.placeholder = "Search for a product..";
+        document.body.prepend(searchBar);
     }
+    
     if (!document.querySelector(".search-button")) {
         const searchButton = document.createElement("button");
         searchButton.className = "search-button";
         searchButton.textContent = "Search";
         document.body.prepend(searchButton);
     }
+    
     if (!document.querySelector(".container")) {
         const container = document.createElement("div");
         container.className = "container";
@@ -150,4 +154,4 @@ document.addEventListener("DOMContentLoaded", () => {
     showAllProducts();
     handleSearch();
     
-})
+});
